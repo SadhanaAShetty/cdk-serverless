640e363dff86345b6a6d93f053efd7713ce67c16
SadhanaAShetty <sadhanashetty30@gmail.com>
2025-05-25 13:35:44 +0200

    updated app.py

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import os
 import aws_cdk as cdk
 from dotenv import load_dotenv
 
-
+# from order_processing.order_processing_frontend_stack import OrderProcessingFrontendStack
+# from order_processing.order_processing_backend_stack import OrderProcessingBackendStack
 from notify_my_turn.notify_my_turn import NotifyMyTurnStack
+
+
 
 
 
@@ -15,20 +18,6 @@ region = os.getenv("AWS_REGION")
 
 app = cdk.App()
 
-stack = NotifyMyTurnStack(app, "NotifyMyTurnStack", 
-                                            env =cdk.Environment(account = account, region = region),
-)
-
-
-
-# backend_stack.add_dependency(frontend_stack)
-app.synth()
-
-
-#OrderProcessing
-# from order_processing.order_processing_frontend_stack import OrderProcessingFrontendStack
-# from order_processing.order_processing_backend_stack import OrderProcessingBackendStack
-
 # frontend_stack = OrderProcessingFrontendStack(app, "OrderProcessingFrontendStack",
 #    env=cdk.Environment(account=account, region=region),
 #     )
@@ -36,4 +25,18 @@ app.synth()
 #    env=cdk.Environment(account=account, region=region)
 #     )
 # backend_stack.add_dependency(frontend_stack)
+
+stack = NotifyMyTurnStack(app, "NotifyMyTurnStack", 
+                                            env =cdk.Environment(account = account, region = region),
+)
+
+
+
+
+app.synth()
+
+
+
+
+
 
