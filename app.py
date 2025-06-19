933af4e0737fe2a763142e7aa1a43e4a21211506
SadhanaAShetty <sadhanashetty30@gmail.com>
2025-06-19 23:30:30 +0200

    Feat/loan processor2 (#5)
    * initial commit
    
    * functioning code for loan processor with stepfunction workflow
    
    * added gitactions workflow
    
    * testing workflow
    
    * updated new location
    
    * updated nodejs
    
    * updated github actions to use OIDC
    
    * commented caching step in workflow
    
    * fixed the npm cache workflow
    
    * commented the if condition for cache installing python dependencies
    
    * commented the cdk deploy to test codebuild feature
    
    * edited the email message in lambda
    
    * updated deploy.yml to test codebuild for simple hello world
    
    * testing the codebuild function
    
    * changed the python version in deploy.yml
    
    * changed the python version to 3.10
    
    * changed the python version to 3.10
    
    * changed the cdk deployment tag in deploy.yml for testing codebuild

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ from dotenv import load_dotenv
 # from order_processing.order_processing_frontend_stack import OrderProcessingFrontendStack
 # from order_processing.order_processing_backend_stack import OrderProcessingBackendStack
 # from notify_my_turn.notify_my_turn import NotifyMyTurnStack
+
 # from loan_processor2.loan_processing_stack import LoanProcessingStack
 # from loan_processor3.loan_processing_stack import LoanProcessingStack
 from image_processing.image_processing_stack import ImageProcessingStack
+# from loan_processing.loan_processing_stack import LoanProcessingStack
+
+
 
 
 load_dotenv()
