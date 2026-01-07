482c02b7cbf8174cf527ba21a82da67e173dbdde
SadhanaAShetty <sadhanashetty30@gmail.com>
2026-01-07 13:59:18 +0100

    authentication setup

--- holiday_home_swap/app/schema.py
+++ holiday_home_swap/app/schema.py
@@ -71,3 +71,15 @@ class SwapMatchResponse(BaseModel):
 
     class Config:
         from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
+
+
+class UserInDB(BaseModel):
+    hashed_password: str
---
+++ holiday_home_swap/app/services/auth.py
@@ -0,0 +1,75 @@
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+import jwt
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
+from pydantic import BaseModel
+from jose import JWTError, jwt 
+from passlib.context import CryptContext
+from app.db import get_db
+from app.model import User, Home, SwapBid
+from app.schema import (
+    UserCreate,
+    UserResponse,
+    HomeCreate,
+    HomeResponse,
+    SwapBidCreate,
+    SwapBidResponse,
+    UserInDB
+)
+
+
+
+# openssl rand -hex 32
+SECRET_KEY = "2e3e01575e1bda9cc135581c575570316f471d1d6ced9a9d822d774a94dffae8"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
+
+
+
+
+
+
+
+
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
+def get_user(db, name: str):
+    if name in db:
+        user_dict = db[name]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(db, name: str, password:str):
+    user = get_user(db, name)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data : dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow()+expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes = 15)
+
+    to_encode.update({"exp" : expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
